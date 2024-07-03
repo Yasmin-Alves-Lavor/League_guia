cbf9c5f8bceaafb041cd9885d73ecbae598f1833
Yasmin Alves Lavor <ylavor135@gmail.com>
2024-07-02 22:55:26 -0300

    Adiciona ajustes para posicionar o nome do campeão abaixo da imagem

--- src/components/ChampionCard/ChampionCard.jsx
+++ src/components/ChampionCard/ChampionCard.jsx
@@ -5,23 +5,30 @@ const ChampionCard = ({ champion }) => {
   if (!champion) return null;
 
   return (
-    <div className="championCard">
-      <div className="championInfo">
-        <img className="championImage" src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`} alt={champion.name} />
-        <div className="championDetails">
-          <h2 className="championName">{champion.name}</h2>
-          <p className="championTitle">{champion.title}</p>
-          <p className="championBlurb">{champion.blurb}</p>
-        </div>
+    <div className="champion-card">
+      <div className="champion-image-container">
+        <img 
+          className="champion-image" 
+          src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`} 
+          alt={champion.name} 
+        />
+      </div>
+      <div className="champion-details">
+        <h2 className="champion-name">{champion.name}</h2>
+        <p className="champion-title">{champion.title}</p>
       </div>
       {champion.spells ? (
-        <div className="championAbilities">
+        <div className="champion-abilities">
           {champion.spells.map((spell, index) => (
-            <div key={index} className="championAbility">
-              <img className="abilityImage" src={`http://ddragon.leagueoflegends.com/cdn/14.12.1/img/spell/${spell.image.full}`} alt={spell.name} />
-              <div className="abilityDetails">
-                <p className="abilityName">{spell.name}</p>
-                <p className="abilityDescription">{spell.description}</p>
+            <div key={index} className="champion-ability">
+              <img 
+                className="ability-image" 
+                src={`http://ddragon.leagueoflegends.com/cdn/14.12.1/img/spell/${spell.image.full}`} 
+                alt={spell.name} 
+              />
+              <div className="ability-details">
+                <p className="ability-name">{spell.name}</p>
+                <p className="ability-description">{spell.description}</p>
               </div>
             </div>
           ))}
@@ -34,6 +41,8 @@ const ChampionCard = ({ champion }) => {
 };
 
 export default ChampionCard;
+
+
 
 
 
